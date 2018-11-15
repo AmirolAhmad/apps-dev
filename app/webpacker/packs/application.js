c49410ad7af5d26ddcbadbb43dca786ce36dd1b5
AmirolAhmad <dev.amirol@gmail.com>
2018-11-15 14:34:34 +0800

    configure webpacker, remove sprocket from rails, assets handle by webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,4 +7,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+import 'bootstrap/dist/js/bootstrap';
+import '../src/javascripts/style';
+
+Rails.start();
+Turbolinks.start();
