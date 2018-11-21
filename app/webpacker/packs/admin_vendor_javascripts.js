b44e5018c06f22f0ed9c2b76484cfe7d2f0a6ae1
AmirolAhmad <dev.amirol@gmail.com>
2018-11-21 13:24:02 +0800

    added custom style for admin dashboard

---
+++ app/webpacker/packs/admin_vendor_javascripts.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/js/bootstrap';
+import 'holderjs'
+import '../src/javascripts/admin/vendor/jquery-browser-mobile/jquery.browser.mobile';
+import '../src/javascripts/admin/vendor/popper/popper.min';
+import '../src/javascripts/admin/vendor/bootstrap-datepicker/bootstrap-datepicker';
+import '../src/javascripts/admin/vendor/common/common';
+import '../src/javascripts/admin/vendor/nanoscroller/nanoscroller';
+import '../src/javascripts/admin/vendor/magnific-popup/jquery.magnific-popup';
+import '../src/javascripts/admin/vendor/jquery-placeholder/jquery-placeholder';
+import '../src/javascripts/admin/vendor/jquery-ui/jquery-ui';
+import '../src/javascripts/admin/vendor/jqueryui-touch-punch/jqueryui-touch-punch';
+import '../src/javascripts/admin/vendor/jquery-appear/jquery-appear';
+// import '../src/javascripts/admin/vendor/bootstrap-multiselect/bootstrap-multiselect';
+// specific js add here
+import '../src/javascripts/admin/theme';
+import '../src/javascripts/admin/custom';
+import '../src/javascripts/admin/theme_init';
