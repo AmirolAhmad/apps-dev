d51d54be497fcb68aad174880ea143a8bcbb9a8c
AmirolAhmad <dev.amirol@gmail.com>
2018-11-21 10:03:24 +0800

    added locales, en and ms

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  get '/dashboard' => 'dashboards#index'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    unlocks: 'users/unlocks'
-  }
-  devise_scope :user do
-    get '/login' => 'users/sessions#new'
-    get '/register' => 'users/registrations#new'
-    get '/settings' => 'users/registrations#edit'
+  scope "(:locale)", locale: /en|ms/ do
+    get '/dashboard' => 'dashboards#index'
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      confirmations: 'users/confirmations',
+      passwords: 'users/passwords',
+      unlocks: 'users/unlocks'
+    }
+    devise_scope :user do
+      get '/login' => 'users/sessions#new'
+      get '/register' => 'users/registrations#new'
+      get '/settings' => 'users/registrations#edit'
+    end
+    root 'pages#index'
   end
-  root 'pages#index'
 end
